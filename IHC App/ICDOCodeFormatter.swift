9d8c2f5685bad75b5a1c5f97490d3ea59fc359cb
Vikram <dr.vikramraj87@gmail.com>
2016-06-11 21:48:13 +0530

    ICDOCodeFormatter: stringFromICDOCode(_:) Returns String instead of String?. DocBlocs added

--- IHC App/ICDOCodeFormatter.swift
+++ IHC App/ICDOCodeFormatter.swift
@@ -1,10 +1,31 @@
 import Cocoa
 
 class ICDOCodeFormatter: NSFormatter {
+    /// Regex Pattern for validating ICD-O Code.
+    /// Should start with a number, not zero.
+    /// Followed by three numbers.
+    /// Followed by backslash(/).
+    /// Followed by a number in 0, 1, 2, 3, 6.
+    ///
+    /// # Examples
+    /// ## Valid ones
+    /// - 8000/1
+    /// - 9099/3
+    /// - 8088/0
+    /// ## Invalid ones
+    /// - 0123/1
+    /// - 8088/7
     lazy var regex: NSRegularExpression = {
         return try! NSRegularExpression(pattern: "^([1-9]\\d{3})\\/([01236])$", options: [])
     }()
     
+    /**
+        Returns `ICDOCode` object from string if the string is a valid ICD-O code
+        else returns nil.
+        
+        - Parameter string: ICD-O code as string
+        - Returns: `ICDOCode` object if string is valid else `nil`
+     */
     func icdoCodeFromString(string: String) -> ICDOCode? {
         var icdoCodeObject: AnyObject?
         var errorDescription: NSString?
@@ -18,9 +39,17 @@ class ICDOCodeFormatter: NSFormatter {
         return icdoCode
     }
     
-    func stringFromICDOCode(icdoCode: ICDOCode) -> String? {
-        return stringForObjectValue(icdoCode)
+    /**
+        Returns string from `ICDOCode`
+     
+        - Parameter icdoCode: ICDOCode object
+        - Returns: String representation of ICDOCode
+     */
+ 
+    func stringFromICDOCode(icdoCode: ICDOCode) -> String {
+        return stringForObjectValue(icdoCode)!
     }
+    
     // MARK: - NSFormatter
     override func stringForObjectValue(obj: AnyObject) -> String? {
         guard let icdoCode = obj as? ICDOCode else {
